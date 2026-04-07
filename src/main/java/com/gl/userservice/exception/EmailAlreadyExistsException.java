ed551fb94a0b32b125e0c9a1d19da431fb71fc6d
AdepuShreya30 <adepu.shreya@globallogic.com>
2026-04-07 13:29:49 +0530

    Delete src directory

--- src/main/java/com/gl/userservice/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.gl.userservice.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/gl/userservice/exception/InvalidUpdateException.java
+++
@@ -1,7 +0,0 @@
-package com.gl.userservice.exception;
-
-public class InvalidUpdateException extends RuntimeException {
-    public InvalidUpdateException(String message) {
-        super(message);
-    }
-}
